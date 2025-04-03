c347afc068789b49598711b1eee2e6b8ee511a02
Bridget Amana <amanabridget03@gmail.com>
2025-04-03 23:50:39 +0100

    feat: add sign-up functionality with SignUpForm component

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignUpForm from '../components/auths/SignUpForm'
+
+const SignUp = () => {
+  return (
+    <div>
+        <SignUpForm />
+    </div>
+  )
+}
+
+export default SignUp
