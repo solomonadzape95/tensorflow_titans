67493b335f8289b8393a6e7b17db642f5696bed8
Stephen <praiseadeniji2017@gmail.com>
2025-04-07 20:11:25 +0100

    fix(router): relocate BrowserRouter to main.tsx
    Moved <BrowserRouter> from App.tsx to main.tsx to prevent the "You cannot render a <Router> inside another <Router>" error. Ensures only one Router is used across the app entry point.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
-
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { Routes, Route } from "react-router";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-
-        <Route path="*" element={<Login />} />
-      </Routes>
-    </Router>
-
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="*" element={<Login />} />
+    </Routes>
   );
 }
 
