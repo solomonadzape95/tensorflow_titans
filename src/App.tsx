7226eee96024bfd190c5f4a8e24dbf3e18d78a33
solomonadzape <adzapesolomon@gmail.com>
2025-04-09 01:57:26 +0100

    refactor: changed color for dashboard layout and added dark theme functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { Routes, Route } from "react-router";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import "./App.css";
+import Dashboard from "./pages/dashboard/DashboardLayout";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+
       <Route path="*" element={<Login />} />
     </Routes>
   );
