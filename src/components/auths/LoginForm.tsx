4d2e059b0fcd587e23cc00195b3c8a3ea2da3735
solomonadzape <adzapesolomon@gmail.com>
2025-04-14 21:08:48 +0100

    feat: enhance Login and SignUp forms with Card component and password confirmation

--- src/components/auths/LoginForm.tsx
+++ src/components/auths/LoginForm.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Link } from "react-router";
+import { Card } from "../ui/card";
 
 const LoginForm = () => {
   const form = useForm({
@@ -30,7 +31,7 @@ const LoginForm = () => {
 
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center bg-[radial-gradient(circle_at_top_right,#4f32ff26,transparent_90%),radial-gradient(circle_at_bottom_left,#f51d7826,transparent_50%)]">
-      <div className="rounded-xl bg-white/40 mx-auto w-full max-w-md shadow-sm backdrop-blur-2xl">
+      <Card className="rounded-xl bg-white/40 mx-auto w-full max-w-md shadow-lg backdrop-blur-2xl">
         <div className="flex flex-col p-6 space-y-1">
           <div className="flex items-center justify-center gap-2">
             <svg
@@ -106,6 +107,7 @@ const LoginForm = () => {
                     </div>
                     <FormControl>
                       <Input
+                        placeholder="********"
                         type="password"
                         className="border border-white/20 bg-white backdrop-blur-md focus:border-black/10"
                         {...field}
@@ -118,22 +120,12 @@ const LoginForm = () => {
 
               <Button
                 type="submit"
-                className="rounded-3xl text-sm font-medium bg-gradient-to-r from-[#4F32FF] to-[#ff4ecd] text-white w-full cursor-pointer"
+                className="rounded-3xl text-sm font-medium bg-gradient-to-r from-[#4F32FF] to-[#ff4ecd] text-white w-full cursor-pointer hover:shadow-md"
               >
                 Sign In
               </Button>
             </form>
           </Form>
-
-          <div className="text-center">
-            <Button
-              variant="link"
-              className="text-[#4F32FF] font-semibold underline-offset-4 hover:underline px-4 py-2 text-sm"
-            >
-              Use demo account
-            </Button>
-          </div>
-
           <div className="flex justify-center items-center text-xs uppercase">
             <hr className="h-px text-[#D6D9E0] w-full" />
             <span className="bg-[#f5f7f9] px-2 text-[#6b7280] w-full">
@@ -142,31 +134,10 @@ const LoginForm = () => {
             <hr className="h-px text-[#D6D9E0] w-full" />
           </div>
 
-          <div className="grid grid-cols-2 gap-4">
+          <div className="grid grid-cols-1">
             <Button
               variant="outline"
-              className="bg-white/70 backdrop-blur-md border border-white/20 text-[#030712]"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
-                <path d="M9 18c-4.51 2-5-2-7-2"></path>
-              </svg>
-              Github
-            </Button>
-            <Button
-              variant="outline"
-              className="bg-white/70 backdrop-blur-md border border-white/20 text-[#030712]"
+              className="bg-white/70 hover:bg-white/70 cursor-pointer hover:shadow-md backdrop-blur-md border border-white/20 text-[#030712]"
             >
               <svg className="mr-2 h-4 w-4" viewBox="0 0 24 24">
                 <path
@@ -198,10 +169,9 @@ const LoginForm = () => {
             </div>
           </div>
         </div>
-      </div>
+      </Card>
     </div>
   );
 };
 
 export default LoginForm;
-
